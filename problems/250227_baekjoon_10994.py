8bd265039ef5f051c068e6bbd89bf58255a877d9
김영은 <yevelop10@gmail.com>
2025-02-28 14:27:01 +0900

    20250228

--- problems/250227_baekjoon_10994.py
+++ problems/250227_baekjoon_10994.py
@@ -6,5 +6,28 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input())
+# d 시작점
+def fillBoxes(d, size, box):
 
+    # 가로
+    for i in range(size-(2*d)):
+        box[d][d+i] = '*'
+        box[size-d-1][d+i] = '*'
+
+    # 세로
+    if size-(2*d) > 1:
+        for i in range(1, size-(2*d)):
+            box[d+i][d] = '*'
+            box[d+i][size-d-1] = '*'
+
+    return box
+
+n = int(input())
+size = (4 * n) - 3
+box = [[' ']*size for _ in range(size)]
+
+for d in range(0, n):
+    box = fillBoxes(d*2, size, box)
+
+for b in box:
+    print(''.join(b))
